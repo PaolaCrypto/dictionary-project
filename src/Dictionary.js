2ad9707f4432e2a319cab77e83b5ef8b8178c8ff
PaolaCrypto <paolaseverino@protonmail.com>
2022-04-21 16:35:08 -0400

    add photos to search words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,20 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function PexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,6 +24,13 @@ export default function Dictionary() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000010a2a833279184c26917677b549db9fde";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
+    axios.get(pexelsUrl, { headers: headers }).then(PexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -32,6 +46,7 @@ export default function Dictionary() {
         <div className="hint">suggested words: sunset, radio, home</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
